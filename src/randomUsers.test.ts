88e16dd974f66e66ad4e23e75be9f3ab5d4add7c
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-12 20:04:05 +0100

    wycofanie raportowania

--- src/randomUsers.test.ts
+++ src/randomUsers.test.ts
@@ -1,62 +1,25 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
+import { Options } from "k6/options";
 
-export const options = {
-
+export const options: Options = {
   stages: [
     { duration: "10s", target: 1 },
-    { duration: "10s", target: 2 },
-    { duration: "10s", target: 3 }
+    { duration: "10s", target: 3 },
+    { duration: "10s", target: 0 }
   ],
-
   thresholds: {
-    http_req_failed: ["rate<0.01"],
-    http_req_duration: ["p(95)<800"]
+    http_req_duration: ["p(95)<800"],
+    http_req_failed: ["rate<0.01"]
   }
-
 };
 
-// container for request logs
-const requestLogs: any[] = [];
-
 export default function () {
+  const res = http.get("https://randomuser.me/api/");
 
-  const url = "https://randomuser.me/api/?results=5";
-
-  const response = http.get(url);
-
-  check(response, {
+  check(res, {
     "status is 200": (r) => r.status === 200
   });
 
-  // save request log
-  requestLogs.push({
-    timestamp: new Date().toISOString(),
-    vu: __VU,
-    iteration: __ITER,
-    request: {
-      method: "GET",
-      url: url
-    },
-    response: {
-      status: response.status,
-      body: response.body
-    }
-  });
-
   sleep(1);
-}
-
-
-// export test summary and logs
-export function handleSummary(data: any) {
-
-  return {
-
-    "logs/test-run-log.json": JSON.stringify(requestLogs, null, 2),
-
-    stdout: JSON.stringify(data, null, 2)
-
-  };
-
 }
