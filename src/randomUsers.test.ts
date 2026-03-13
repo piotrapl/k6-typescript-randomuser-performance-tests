438e75653075935216cea4edd0251fae4a4bf83d
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-13 20:23:20 +0100

    adding html reporting

--- src/randomUsers.test.ts
+++ src/randomUsers.test.ts
@@ -1,25 +1,44 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
-import { Options } from "k6/options";
 
-export const options: Options = {
+// HTML reporter
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export const options = {
+
   stages: [
     { duration: "10s", target: 1 },
-    { duration: "10s", target: 3 },
-    { duration: "10s", target: 0 }
+    { duration: "10s", target: 2 },
+    { duration: "10s", target: 3 }
   ],
+
   thresholds: {
-    http_req_duration: ["p(95)<800"],
-    http_req_failed: ["rate<0.01"]
+    http_req_failed: ["rate<0.01"],
+    http_req_duration: ["p(95)<800"]
   }
+
 };
 
 export default function () {
-  const res = http.get("https://randomuser.me/api/");
 
-  check(res, {
+  const url = "https://randomuser.me/api/?results=5";
+
+  const response = http.get(url);
+
+  check(response, {
     "status is 200": (r) => r.status === 200
   });
 
   sleep(1);
+
+}
+
+
+// generate report after test
+export function handleSummary(data: any) {
+
+  return {
+    "reports/performance-report.html": htmlReport(data)
+  };
+
 }
---
+++ src/types/k6-html-reporter.d.ts
@@ -0,0 +1,3 @@
+    declare module "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js" {
+  export function htmlReport(data: any): string;
+}
