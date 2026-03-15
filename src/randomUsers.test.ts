21bbe14174857cb128978c9af351a71f4686903a
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-15 19:04:28 +0100

    zmiana scenariuszy testów

--- src/randomUsers.test.ts
+++ src/randomUsers.test.ts
@@ -1,15 +1,49 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
 
-// HTML reporter
+// HTML report generator
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
 
-  stages: [
-    { duration: "10s", target: 1 },
-    { duration: "10s", target: 2 }
-  ],
+  scenarios: {
+
+    sequenceA_singleUser: {
+      executor: "constant-vus",
+      exec: "sequenceA",
+      vus: 1,
+      duration: "10s",
+      tags: { sequence: "A - getting data of 1 random user" }
+    },
+
+    sequenceA_parallelUsers: {
+      executor: "constant-vus",
+      exec: "sequenceA",
+      vus: 7,
+      duration: "10s",
+      startTime: "10s",
+      tags: { sequence: "A - getting data of 1 random user" }
+    },
+
+    sequenceB_singleUser: {
+      executor: "constant-vus",
+      exec: "sequenceB",
+      vus: 1,
+      duration: "10s",
+      startTime: "20s",
+      tags: { sequence: "B - getting data of 7 random users" }
+    },
+
+    sequenceB_parallelUsers: {
+      executor: "constant-vus",
+      exec: "sequenceB",
+      vus: 7,
+      duration: "10s",
+      startTime: "30s",
+      tags: { sequence: "B - getting data of 7 random users" }
+    }
+
+  },
 
   thresholds: {
     http_req_failed: ["rate<0.01"],
@@ -18,22 +52,42 @@ export const options = {
 
 };
 
-export default function () {
 
-  const url = "https://randomuser.me/api/?results=5";
+// Sequence A
+export function sequenceA() {
 
-  const response = http.get(url);
+  const url = "https://randomuser.me/api/?results=1";
+
+  const response = http.get(url, {
+    tags: { endpoint: "results=1" }
+  });
 
   check(response, {
     "status is 200": (r) => r.status === 200
   });
 
   sleep(1);
-
 }
 
 
-// generate report after test
+// Sequence B
+export function sequenceB() {
+
+  const url = "https://randomuser.me/api/?results=7";
+
+  const response = http.get(url, {
+    tags: { endpoint: "results=7" }
+  });
+
+  check(response, {
+    "status is 200": (r) => r.status === 200
+  });
+
+  sleep(1);
+}
+
+
+// generate HTML report
 export function handleSummary(data: any) {
 
   return {
